072c1543da8f15aada079d891cbc0c44acf7453d
gandresr <gandres.r@hotmail.com>
2025-10-27 17:37:39 -0500

    refactor: update metric implementations

--- tempus_bench/metrics/rmse.py
+++ tempus_bench/metrics/rmse.py
@@ -1,13 +1,13 @@
-import pandas as pd
 import numpy as np
-from typing import Dict, Any, Union
+
+from tempus_bench.metrics.base_metric import BaseMetric
 
 """
 Calculates Root Mean Squared Error.
 """
 
 
-class RMSE:
+class RMSE(BaseMetric):
     def __call__(
         self, y_true: np.ndarray, y_pred: np.ndarray, **kwargs
     ) -> float:
@@ -23,15 +23,5 @@ class RMSE:
         Returns:
             The calculated RMSE score.
         """
-
-        task_type = kwargs.get('task_type')
-        if task_type == 'stochastic':
-            point_forecast_statistic = kwargs['point_forecast_statistic']
-            if point_forecast_statistic == 'mean':
-                y_ppred = np.mean(y_pred, axis=0)
-            else:
-                raise ValueError("MASE can only handle point_forecast_statistic == 'mean' for stochastic evaluation.")
-        else:
-            y_ppred = y_pred
-
+        y_ppred = self.process_params(y_true, y_pred, **kwargs)
         return np.sqrt(np.mean((y_true - y_ppred) ** 2))
