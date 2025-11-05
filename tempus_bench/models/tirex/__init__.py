3946e47a2b8164a39e1f00350edbecc19912e4ee
beatriz5 <beatrizdelucio@protonmail.com>
2025-11-05 17:52:18 -0500

    tirex and chronos 2

---
+++ tempus_bench/models/tirex/__init__.py
@@ -0,0 +1,13 @@
+"""
+TiRex
+
+References:
+    - Paper: https://arxiv.org/abs/2505.23719
+    - GitHub: https://github.com/NX-AI/tirex
+    - HuggingFace: https://huggingface.co/NX-AI/TiRex
+"""
+
+from .tirex_model import TirexModel
+
+__all__ = ["TirexModel"]
+
